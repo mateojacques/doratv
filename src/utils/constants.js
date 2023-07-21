c38b78f0eb45ad38e48132e7ff0aed177b5fed1c
Leonel Ochoa <leonel170814@gmail.com>
2023-07-21 15:37:08 -0300

    variables removed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,3 @@
-export const DEFAULT_GAME = {
-  id: "509658",
-  name: "Just Chatting",
-  box_art_url:
-    "https://static-cdn.jtvnw.net/ttv-boxart/509658-{width}x{height}.jpg",
-};
-export const DEFAULT_LANGUAGE = "en";
-
 export const MAX_PANEL_STREAMS = 20;
 export const LOAD_MORE_STREAMS_STEP = 20;
 
